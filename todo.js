43e6ae9abc702004c1d46cc582b8638bb8c3b7a7
Vishal Nayak <vknayak938@gmail.com>
2024-11-07 13:38:26 +0530

    firebase

--- todo.js
+++ todo.js
@@ -1,5 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js";
-import { getDatabase, ref, set, get, onValue } from "https://www.gstatic.com/firebasejs/9.17.2/firebase-database.js";
+
 
 let todoList = [
   {
@@ -20,48 +19,10 @@ function addTodo() {
   let todoItem = inputElement.value;
   let todoDate = dateElement.value;
   todoList.push({item: todoItem, dueDate: todoDate});
-  inputElement.value = '';
-  dateElement.value = '';
-
-  const firebaseConfig = {
-    apiKey: "AIzaSyAo2Yxs2tKSzvEY0oN_ks_cXb2g-M7ra2U",
-    authDomain: "todoapp-8bdb8.firebaseapp.com",
-    databaseURL: "https://todoapp-8bdb8-default-rtdb.firebaseio.com",
-    projectId: "todoapp-8bdb8",
-    storageBucket: "todoapp-8bdb8.firebasestorage.app",
-    messagingSenderId: "591722680070",
-    appId: "1:591722680070:web:d78d543de28cc420705887",
-    measurementId: "G-B8L54TGGQB"
-  };
-  
-     // Initialize Firebase
-     const app = initializeApp(firebaseConfig);
-    //  const db="https://todoapp-8bdb8-default-rtdb.firebaseio.com/"
-  const database=getDatabase(app);
-  
-  // Reference a path in the database
-  const dbRef = ref(database, 'users');
-  
-  // Write data to the database
-  document.querySelector(".btn-todo").addEventListener("click",(e)=>{
-    e.preventDefault()
-    console.log(`this is todo : ${todoItem}`)
-  
-  set(dbRef, {
-    Todo: todoItem,
-    Date: todoItem,
-  })
-  .then(() => {
-    console.log("Data written successfully!");
-  })
-  .catch((error) => {
-    console.error("Error writing data:", error);
-  });
-  })
-  
-  displayItems();
+ todoDate=""
+ todoItem=""
+displayItems()
 }
-
 function displayItems() {
   let containerElement = document.querySelector('.todo-container');
   let newHtml = '';
@@ -76,5 +37,6 @@ function displayItems() {
   }
   containerElement.innerHTML = newHtml;
 }
+
 
 
